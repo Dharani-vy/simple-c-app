4eea1f0d3b97b94e0b5f84074a2e0bcbad4ebbaa
Dharani <dharanivelusamy2003@gmail.com>
2024-07-10 19:53:55 +0530

    1st commit:creating sorting program

---
+++ app.c
@@ -0,0 +1,43 @@
+// app.c
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void sort_numbers(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void process_string(char *str) {
+    int i;
+    for (i = 0; str[i]; i++) {
+        str[i] = tolower(str[i]);
+    }
+    str[i] = '\0';
+}
+
+int main() {
+    int numbers[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(numbers)/sizeof(numbers[0]);
+    char string[] = "   Hello, World!   ";
+
+    sort_numbers(numbers, n);
+    printf("Sorted Numbers: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    process_string(string);
+    printf("Processed String: '%s'\n", string);
+
+    return 0;
+}
